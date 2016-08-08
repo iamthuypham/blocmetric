a88c30e4372932f6b3d1ab27a6482493f3a4061c
Thuy Pham <tpham045@hotmail.com>
2016-08-08 03:32:51 +0000

    Add Host link for email confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Host to send confirmation email
+  config.action_mailer.default_url_options = { :host => 'https://tranquil-cliffs-92853.herokuapp.com' }
 end
